57c38b04a3d01d08e5f9b03b3674d6fca61407f0
Sergio Ricardo Pires de Paula <sergio.ricardojf@gmail.com>
2021-01-06 19:53:55 -0300

    Iniciando estudos Dio

---
+++ Dio/DigitalInovationOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      int numeroDeVezes = 5;
+
+      for (int c = 0; c < numeroDeVezes; c++)
+      {
+        Console.WriteLine($"Bem vindo ao curso de .Net {c}");
+      }
+    }
+  }
+}
